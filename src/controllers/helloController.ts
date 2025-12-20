a3d813c9a83b9b39dcb3bcd9397e3b19c484d83d
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-12-20 16:00:25 +0500

    another commit

--- src/controllers/helloController.ts
+++ src/controllers/helloController.ts
@@ -5,7 +5,7 @@ export function helloWorldHandler(request: Request, response: Response) {
   try {
     response.status(ResponseStatus.OK).json({
       message: ResponseMessages.Success,
-      data: { message: "Hello World 1" },
+      data: { message: "Hello World 2" },
     });
   } catch (error) {
     response
