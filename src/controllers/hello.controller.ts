bafa3953acfc89b72e6af6b9f94dcfd7b3c6c3bd
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-12-23 16:14:11 +0500

    agnet trigger

--- src/controllers/hello.controller.ts
+++ src/controllers/hello.controller.ts
@@ -5,7 +5,7 @@ export function helloWorldHandler(request: Request, response: Response) {
   try {
     response.status(ResponseStatus.OK).json({
       message: ResponseMessages.Success,
-      data: { message: "Hello World 19" },
+      data: { message: "Hello World 20" },
     });
   } catch (error) {
     response
