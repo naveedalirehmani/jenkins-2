5896bbd0d0612c63a79f11217682b8c455ebf0c0
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-12-23 15:03:24 +0500

    update

--- src/controllers/hello.controller.ts
+++ src/controllers/hello.controller.ts
@@ -5,7 +5,7 @@ export function helloWorldHandler(request: Request, response: Response) {
   try {
     response.status(ResponseStatus.OK).json({
       message: ResponseMessages.Success,
-      data: { message: "Hello World 12" },
+      data: { message: "Hello World 13" },
     });
   } catch (error) {
     response
