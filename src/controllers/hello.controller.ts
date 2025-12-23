0d47a29bcd24c4b5810853f47fd3bbca4d3e2d34
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-12-23 15:42:24 +0500

    resetup test

--- src/controllers/hello.controller.ts
+++ src/controllers/hello.controller.ts
@@ -5,7 +5,7 @@ export function helloWorldHandler(request: Request, response: Response) {
   try {
     response.status(ResponseStatus.OK).json({
       message: ResponseMessages.Success,
-      data: { message: "Hello World 13" },
+      data: { message: "Hello World 14" },
     });
   } catch (error) {
     response
