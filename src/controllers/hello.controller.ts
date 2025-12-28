34b1e5ff97cd9b379c326aa0aadd2a5dc3471fc3
naveedalirehmani <naveedalirehmani321@gmail.com>
2025-12-28 19:48:55 +0500

    Update helloWorldHandler response message to "Hello World 27"

--- src/controllers/hello.controller.ts
+++ src/controllers/hello.controller.ts
@@ -5,7 +5,7 @@ export function helloWorldHandler(request: Request, response: Response) {
   try {
     response.status(ResponseStatus.OK).json({
       message: ResponseMessages.Success,
-      data: { message: "Hello World 26" },
+      data: { message: "Hello World 27" },
     });
   } catch (error) {
     response
